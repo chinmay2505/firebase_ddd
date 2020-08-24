c7ba2414ab76a3fce7ba2db45f593debc5d92b7e
chinmay mourya <chinmay.mourya@ksolves.com>
2020-08-24 23:30:12 +0530

    Added bloc structure for auth feature and added the events and states in bloc related to auth feature

--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -4,9 +4,13 @@ import 'package:firebase_ddd/domain/auth/value_objects.dart';
 import 'package:flutter/foundation.dart';
 
 abstract class IAuthFacade {
-  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword(
-      {@required EmailAddress emailAddress, @required Password password});
-  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword(
-      {@required EmailAddress emailAddress, @required Password password});
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
   Future<Either<AuthFailure, Unit>> signInWithGoogle();
 }
